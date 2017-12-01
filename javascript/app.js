a9197e646c7d4a5bfd2d2ee0537668bfb77770be
boginis1 <hollyzoba@gmail.com>
2017-11-30 17:26:55 -0700

    draft

---
+++ javascript/app.js
@@ -0,0 +1,41 @@
+var triviaQandA = [{
+		"Question": "Creating awareness or questioning the status quo is the strategy for which stage of the customer journey",
+		"Answers":["Inspire", "Guide", "Captivate", "Persuade"]
+}
+{
+		"Question": "Which of these tools is NOT likely to be used to create awareness?",
+		"Answers":["Videos", "Proposals", "Social Forums", "Email Blasts"]}
+}
+
+{
+		"Question": "The primary intent in sending a prospecting email is what?",
+		"Answers":["Create awareness", "Sell", "Invite for a site inspection", "Get a referral"]
+}
+{
+		"Question": "When writing business correspondence, at what grade level should we be writing?",
+		"Answers":["4th - 6th", "6th to 8th", "8th-10th", "10th-12th"]}
+}
+{
+		"Question": "How many people are typically involved in the average purchase decision today?",
+		"Answers":["3.4", "4.5", "6.8", "12"]
+}
+{
+		"Question": "The initials AAPS stand for this which we should use when responding to prospect questions",
+		"Answers":["Acknowledge, Assure, Persuade, Substantiate", "Assure, Ask, Persuade, Sell", "Acknowledge, Ask, Present, Sell", "Ask, Acknowledge, Persuade, Substantiate"]}
+}
+{
+		"Question": "When your prospect is in the decision phase of their journey, what should we do as sellers?",
+		"Answers":["Inspire", "Guide", "Captivate", "Persuade"]
+}
+{
+		"Question": "What percent of the buyer's journey is typically completed before a salesperson gets involved?",
+		"Answers":["40%", "50%", "60%", "more than 60%"]}
+}
+{
+		"Question": "When our prospects are exploring, what strategy should we take as sellers?",
+		"Answers":["Inspire", "Guide", "Captivate", "Persuade"]
+}
+{
+		"Question": "When we are in the captivate phase, what is our specific strategy?",
+		"Answers":["Be a valuable resource", "Be first and be fabulous", "Create memorable", "Create awareness"]}
+}];
