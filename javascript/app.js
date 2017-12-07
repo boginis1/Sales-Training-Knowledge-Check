e75d038bcd68f1d598b6c8ee9c0415db154ae473
boginis1 <hollyzoba@gmail.com>
2017-12-07 16:56:39 -0700

    update 200

--- javascript/app.js
+++ javascript/app.js
@@ -1,182 +1,159 @@
 $(document).ready(function() {
-  // Handler for .ready() called.
- var questions = [
-                new Question("Creating awareness or questioning the status quo is the strategy for which stage of the customer journey?", ["Inspire", "Guide", "Captivate", "Persuade"],
-                    "Inspire",
-                    "We want to INSPIRE our potential buyers to create awareness or question their status quo."
-                ),
-                new Question("Which of these tools is NOT likely to be used to create awareness?", ["Videos", "Proposals", "Social Forums", "Email Blasts"],
-                    "Proposals",
-                    "Proposals are not generally used to create awareness, but instead to persuade when the buyer is in the decision phase."
-                ),
+    // Handler for .ready() called.
+    var triviaQandA = [{
+            "question": "Creating awareness or questioning the status quo is the strategy for which stage of the customer journey?",
+            "answers": ["Inspire", "Guide", "Captivate", "Persuade"],
+            "correct": "Inspire",
+            "response": "We want to INSPIRE our potential buyers to create awareness or question their status quo."
+        },
+        {
+            "question": "Which of these tools is NOT likely to be used to create awareness?",
+            "answers": ["Videos", "Proposals", "Social Forums", "Email Blasts"],
+            "correct": "Proposals",
+            "response": "Proposals are not generally used to create awareness, but instead to persuade when the buyer is in the decision phase."
+        },
+        {
 
-                new Question("The primary intent in sending a prospecting email is what?", ["Create awareness", "Sell", "Invite for a site inspection", "Get a referral"],
-                    "Get a referral",
-                    "Getting a referral is what we hope to accomplish by sending a prospecting email."
-                ),
-                new Question("When writing business correspondence, at what grade level should we be writing?", ["4th - 6th", "6th to 8th", "8th-10th", "10th-12th"],
-                    "6th to 8th",
-                    "6th to 8th grade is the ideal level for the most clear and compelling business writing."
-                ),
-                new Question("How many people are typically involved in the average purchase decision today?", ["3.4", "4.5", "6.8", "12"],
-                    "6th to 8th",
-                    "6.8 people are now typically involved in the average purchase decision today."
-                ),
-                new Question("The initials AAPS stand for this which we should use when responding to prospect questions", ["Acknowledge, Assure, Persuade, Substantiate", "Assure, Ask, Persuade, Sell", "Acknowledge, Ask, Present, Sell", "Ask, Acknowledge, Persuade, Substantiate"],
-                    "Acknowledge, Assure, Persuade, Substantiate",
-                    "Acknowledge, Assure, Persuade and Substantiate is the best practice in writing a compelling email response."
-                ),
-                new Question("When your prospect is in the decision phase of their journey, what should we do as sellers?", ["Inspire", "Guide", "Captivate", "Persuade"],
-                    "Persuade",
-                    "We want to PERSUADE them that our product or solution aligns best with their needs."
-                ),
-                new Question("What percent of the buyer's journey is typically completed before a salesperson gets involved?", ["40%", "50%", "60%", "more than 60%"],
-                    "more than 60%",
-                    "More than 60% - as much as 80% of the purchase journey is complete before a buyer will typically talk to a salesperson."
-                ),
-                new Question("When our prospects are exploring, what strategy should we take as sellers?", ["Inspire", "Guide", "Captivate", "Persuade"],
-                    "Guide",
-                    "We want to GUIDE our potential buyers by positioning ourselves as a valuable resource."
-                ),
-                new Question("When we are in the captivate phase, what is our specific strategy?", ["Be a valuable resource", "Be first and be fabulous", "Create memorable", "Create awareness"],
-                    "Be first and be fabulous",
-                    "We want to be FIRST AND BE FABULOUS because this is our best opportunity to stand apart from our competitors."
-                ),
-            ];
+            "question": "The primary intent in sending a prospecting email is what?",
+            "answers": ["Create awareness", "Sell", "Invite for a site inspection", "Get a referral"],
+            "correct": "Get a referral",
+            "response": "Getting a referral is what we hope to accomplish by sending a prospecting email."
+        },
+        {
+            "question": "When writing business correspondence, at what grade level should we be writing?",
+            "answers": ["4th - 6th", "6th to 8th", "8th-10th", "10th-12th"],
+            "correct": "6th to 8th",
+            "response": "6th to 8th grade is the ideal level for the most clear and compelling business writing."
+        },
+        {
+            "question": "How many people are typically involved in the average purchase decision today?",
+            "answers": ["3.4", "4.5", "6.8", "12"],
+            "correct": "6th to 8th",
+            "response": "6.8 people are now typically involved in the average purchase decision today."
+        },
+        {
+            "question": "The initials AAPS stand for this which we should use when responding to prospect questions",
+            "answers": ["Acknowledge, Assure, Persuade, Substantiate", "Assure, Ask, Persuade, Sell", "Acknowledge, Ask, Present, Sell", "Ask, Acknowledge, Persuade, Substantiate"],
+            "correct": "Acknowledge, Assure, Persuade, Substantiate",
+            "response": "Acknowledge, Assure, Persuade and Substantiate is the best practice in writing a compelling email response."
+        },
+        {
+            "question": "When your prospect is in the decision phase of their journey, what should we do as sellers?",
+            "answers": ["Inspire", "Guide", "Captivate", "Persuade"],
+            "correct": "Persuade",
+            "response": "We want to PERSUADE them that our product or solution aligns best with their needs."
+        },
+        {
+            "question": "What percent of the buyer's journey is typically completed before a salesperson gets involved?",
+            "answers": ["40%", "50%", "60%", "more than 60%"],
+            "correct": "more than 60%",
+            "response": "More than 60% - as much as 80% of the purchase journey is complete before a buyer will typically talk to a salesperson."
+        },
+        {
+            "question": "When our prospects are exploring, what strategy should we take as sellers?",
+            "answers": ["Inspire", "Guide", "Captivate", "Persuade"],
+            "correct": "Guide",
+            "response": "We want to GUIDE our potential buyers by positioning ourselves as a valuable resource."
+        },
+        {
+            "question": "When we are in the captivate phase, what is our specific strategy?",
+            "answers": ["Be a valuable resource", "Be first and be fabulous", "Create memorable", "Create awareness"],
+            "correct": "Be first and be fabulous",
+            "response": "We want to be FIRST AND BE FABULOUS because this is our best opportunity to stand apart from our competitors."
+        }
 
-            var quiz = new Quiz(questions);
+    ];
 
-            populate();
-            function populate() {
-                if (quiz.isEnded()) {
-                    showScore();
+    var questionIndex = 0;
+    var correctAnswer = 0
+    var wrongAnswer = 0
+    var timeOutAnswer = 0
 
-                } else {
-                    //show questions
-                    var element = $("#question");
-                    element.innerHTML = quiz.getQuestionIndex().text;
-                }
-                //show choices for answers
-                var answers = quiz.getQuestionIndex().answers;
-                for (var i = 0; i < answers.length; i++) {
-                    var element = $("#answers" + i);
-                    element.innerHTML = answers[i];
-                    guess("btn" + i, answers[i]);
-                }
-                showProgress();
+    //Timer program
+    var interValID = setInterval(timeIt, 1000);
 
-            };
+    var counter = 0
+    var timeLeft = 30
+    var timer = $("#timer");
+    timer.html("<h2>You have " + (timeLeft - counter) + " seconds left</h2>");
 
-            function guess(id, guess) {
-                var button = $("#id");
-                button.onclick = function() {
-                    quiz.guess(guess);
-                    populate();
-                }
-            }
-            //show choices
+    function timeIt() {
+        counter++
+        timer.html("<h2>You have " + (timeLeft - counter) + " seconds left</h2>");
+        if (counter == timeLeft) {
+            counter = 0
+            timer.html("<h2>Your time is up!</h2>");
+            clearInterval(interValID);
+        }
+    }
 
-            function showProgress() {
-                var currentQuestionNumber = quiz.questionIndex + 1;
+
+    $("#startGame").click(function() {
+        getQuestion();
+    });
+
+function showProgress() {
+                var currentQuestionNumber = questionIndex + 1;
                 var element = $("#progress");
-                element.innerHTML = "Questiom " + currentQuestionNumber + "of" + quiz.questions.length;
+                element.innerHTML = "Question " + (currentQuestionNumber) + "of" + question.length;
             }
 
-            function showScores() {
-                var gameOverHtml = "<h1>Result</h1>";
-                gameOverHtml += "<h2> Your score: " + quiz.score + "</h2>";
-                var element = $("#quiz");
-                element.innerHTML = gameOverHtml;
-            }
+    function getQuestion() {
+        //var a = 0;
+        $("#startTheGame").css("visibility", "hidden");
+        $("#startGame").css("visibility", "hidden");
+        $(".grid").css("visibility", "visible");
+        timeIt(30, 1000);
+        var selection = triviaQandA[questionIndex];
+        this.question = selection["question"];
+        this.answers = selection["answers"];
+        this.correct = selection["correct"];
+        this.response = selection["response"];
+        $("#question").html(selection["question"]);
+        var answers = selection.answers;
+        
+        for (var i = 0; i < answers.length; i++) {
+            btn = $("button#btn" + [i]).append(answers[i]);
+                
+                console.log (btn + [i]);
+        }
+        showProgress();
+        questionIndex++
 
-            //Timer program
-
-            var counter = 0
-            var timeLeft = 5
-            var timer = $("#timer");
-            timer.html("<h2>You have " + (timeLeft - counter) + " seconds left</h2>");
-
-            function timeIt() {
-                counter++
-                timer.html("<h2>You have " + (timeLeft - counter) + " seconds left</h2>");
-                if (counter == timeLeft) {
-                    counter = 0
-                    timer.html("<h1>Your time is up!</h1>");
-
-                }
-
-            }
-            var interValID = setInterval(timeIt, 1000);
+    }
 
 
-            //stop the timer
-            function myStopFunction() {
-                clearInterval(interValID, 3000);
-                timeIt();
-            }
+    $("button").click(function() {
+        
 
-            $("#startTheGame.button").click(function() {
-                startGame();
-            })
-
-            //start the game
-            function startGame() {
-                myStopFunction();
-                var startGame = $("#startTheGame")
-                $(".startTheGame").css("visibility", "hidden");
-                $("#grid").css("visibility", "visible");
-                getQuestion();
-                timeIt();
+        clearInterval(interValID);
 
 
-            }
 
-            // begin questions and answers -- name parameters in our questions class
-            //.....................................................................
-            function Question(text, answers, correct, response) {
-                this.text = text;
-                this.answers = answers;
-                this.correct = correct;
-                this.response = response;
+    });
 
-            }
+    
 
-            Question.prototype.correctAnswer = function(answers) {
-                return answers === this.correct;
-            }
+    function checkAnswer() {
+        
+        if ($("button").hasClass("winner")) {
+            correctAnswer++
+            console.log (winner);
+            $("#response").html("You are correct!" + response);
+        } else if (counter == timeLeft) {
+            $("#response").html("You ran out of time" + response);
+            timeOutAnswer++
+        }
+            else 
+        {
 
+            $("#response").html("You are incorrect!" + response);
+            wrongAnswer++
+        }
 
-            //select answer
-            function Quiz(questions) {
-                this.score = 0;
-                this.questions = questions;
-                this.questionIndex = 0;
-            }
+        getQuestion();
 
+    }
+ 
 
-            Quiz.prototype.getQuestionIndex = function() {
-                return this.questions[this.questionIndex];
-            }
-            Quiz.prototype.isEnded = function() {
-                return this.questions.length === this.questionIndex;
-            }
-            Quiz.prototype.guess = function(correct) {
-
-                if (this.getQuestionIndex().correctAnswer(correct) {
-                        this.questionIndex++;
-
-                        if (this.getQuestionIndex().correctAnswer(correct)) {
-                            this.score++;
-                        }
-                        this.questionindex++
-                    }
-                });
-
-                // show response
-
-
-                // start over  
 });
-            
-
-          
