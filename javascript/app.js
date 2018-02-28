2763f1b16a71d61b4464848a038dd5d9d4cd5458
boginis1 <hollyzoba@gmail.com>
2018-02-28 08:12:04 -0700

    changed options and timing

--- javascript/app.js
+++ javascript/app.js
@@ -5,63 +5,64 @@ $(document).ready(function() {
 
     var triviaQandA = [{
             "question": "Creating awareness or questioning the status quo is the strategy for which stage of the customer journey?",
-            "answers": ["Inspire", "Guide", "Captivate", "Persuade"],
-            "correct": "Inspire",
+            "answers": ["A - Inspire", "B - Guide", "C - Captivate", "D - Persuade"],
+            "correct": "A - Inspire",
             "response": "We want to INSPIRE our potential buyers to create awareness or question their status quo."
         },
         {
             "question": "Which of these tools is NOT likely to be used to create awareness?",
-            "answers": ["Videos", "Proposals", "Social Forums", "Email Blasts"],
-            "correct": "Proposals",
+            "answers": ["A - Videos", "B - Proposals", "C - Social Forums", "D - Email Blasts"],
+            "correct": "B-Proposals",
             "response": "Proposals are not generally used to create awareness, but instead to persuade when the buyer is in the decision phase."
         },
         {
 
             "question": "The primary intent in sending a prospecting email is what?",
-            "answers": ["Create awareness", "Sell", "Invite for a site inspection", "Get a referral"],
-            "correct": "Get a referral",
+            "answers": ["A - Create awareness", "B - Sell", "C - Invite for a site inspection", "D - Get a referral"],
+            "correct": "D - Get a referral",
             "response": "Getting a referral is what we hope to accomplish by sending a prospecting email."
         },
         {
             "question": "When writing business correspondence, at what grade level should we be writing?",
-            "answers": ["4th - 6th", "6th to 8th", "8th-10th", "10th-12th"],
-            "correct": "6th to 8th",
+            "answers": ["A - 4th - 6th", "B - 6th to 8th", "C - 8th-10th", "D - 10th-12th"],
+            "correct": "B - 6th to 8th",
             "response": "6th to 8th grade is the ideal level for the most clear and compelling business writing."
         },
         {
             "question": "How many people are typically involved in the average purchase decision today?",
-            "answers": ["3.4", "4.5", "6.8", "12"],
-            "correct": "6.8",
+            "answers": ["A - 3.4", "B - 4.5", "C - 6.8", "D - 12"],
+            "correct": "C - 6.8",
             "response": "6.8 people are now typically involved in the average purchase decision today."
         },
         {
             "question": "The initials AAPS stand for this which we should use when responding to prospect questions",
-            "answers": ["Acknowledge, Assure, Persuade, Substantiate", "Assure, Ask, Persuade, Sell", "Acknowledge, Ask, Present, Sell", "Ask, Acknowledge, Persuade, Substantiate"],
-            "correct": "Acknowledge, Assure, Persuade, Substantiate",
+            "answers": ["A - Acknowledge, Assure, Persuade, Substantiate", 
+            "B - Assure, Ask, Persuade, Sell", "C - Acknowledge, Ask, Present, Sell", "D - Ask, Acknowledge, Persuade, Substantiate"],
+            "correct": "A - Acknowledge, Assure, Persuade, Substantiate",
             "response": "Acknowledge, Assure, Persuade and Substantiate is the best practice in writing a compelling email response."
         },
         {
             "question": "When your prospect is in the decision phase of their journey, what should we do as sellers?",
-            "answers": ["Inspire", "Guide", "Captivate", "Persuade"],
-            "correct": "Persuade",
+            "answers": ["A - Inspire", "B - Guide", "C - Captivate", "D - Persuade"],
+            "correct": "D - Persuade",
             "response": "We want to PERSUADE them that our product or solution aligns best with their needs."
         },
         {
             "question": "What percent of the buyer's journey is typically completed before a salesperson gets involved?",
-            "answers": ["40%", "50%", "60%", "more than 60%"],
-            "correct": "more than 60%",
+            "answers": ["A - 40%", "B - 50%", "C - 60%", "D - more than 60%"],
+            "correct": "D - more than 60%",
             "response": "More than 60% - as much as 80% of the purchase journey is complete before a buyer will typically talk to a salesperson."
         },
         {
             "question": "When our prospects are exploring, what strategy should we take as sellers?",
-            "answers": ["Inspire", "Guide", "Captivate", "Persuade"],
-            "correct": "Guide",
+            "answers": ["A - Inspire", "B - Guide", "C - Captivate", "D - Persuade"],
+            "correct": "B - Guide",
             "response": "We want to GUIDE our potential buyers by positioning ourselves as a valuable resource."
         },
         {
             "question": "When we are in the captivate phase, what is our specific strategy?",
-            "answers": ["Be a valuable resource", "Be first and be fabulous", "Create memorable", "Create awareness"],
-            "correct": "Be first and be fabulous",
+            "answers": ["A - Be a valuable resource", "B - Be first and be fabulous", "C - Create memorable", "D - Create awareness"],
+            "correct": "B - Be first and be fabulous",
             "response": "We want to be FIRST AND BE FABULOUS because this is our best opportunity to stand apart from our competitors."
         }
 
@@ -74,7 +75,7 @@ $(document).ready(function() {
 
     //Timer program
     var interValID;
-    var timeLeft = 20
+    var timeLeft = 30
     var counter = 0;
     var timer = $("#timer");
 
@@ -174,7 +175,7 @@ $(document).ready(function() {
             } else  {
                 counter=0;
                 
-                setTimeout(getQuestion, 4000);
+                setTimeout(getQuestion, 5000);
 
             }
     }
